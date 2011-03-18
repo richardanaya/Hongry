57b20118851baffde7ccd2623e88758d8501e211
Richard Anaya <richard.anaya@gmail.com>
2011-03-17 21:01:20 -0500

    adding titanium and missing images

---
+++ titanium/Hongry/build/iphone/Classes/defines.h
@@ -0,0 +1,10 @@
+// Warning: this is generated file. Do not modify!
+
+#define TI_VERSION 1.6.0
+#define USE_TI_ANALYTICS 1
+#define USE_TI_NETWORK 1
+#define USE_TI_PLATFORM 1
+#define USE_TI_UI 1
+#define USE_TI_UISETBACKGROUNDCOLOR 1
+#define USE_TI_UIWEBVIEW 1
+#define USE_TI_UIWINDOW 1
